4aa587c8dbf2fa62903f8c5b3595e0a36faefd99
Rebekah <rebekah@azimuthinternet.com>
2012-11-26 16:36:48 -0800

    Added devise views and a few other things

--- spec/requests/landing_page_spec.rb
+++ spec/requests/landing_page_spec.rb
@@ -6,6 +6,7 @@ describe "When I visit the home page of the website" do
 	
 	before do
 		visit root_path
+		page!
 	end
 	
 	
@@ -13,7 +14,6 @@ describe "When I visit the home page of the website" do
 		
 		it "I can see a login form" do
 			should have_selector('form[name = "login"]')
-			
 		end	
 		
 		it "and I can also see a register now button" do
